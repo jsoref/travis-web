83d4f5b1d0341e195e9d9b37108daff8d8a74d9d
Curtis Ekstrom <code@fauxton.io>
2018-10-04 15:31:42 +0200

    Silence more deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,7 +1,9 @@
 /* global window */
-window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow = self.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'macro-computed-deprecated' },
+    { handler: "silence", matchId: "macro-computed-deprecated" },
+    { handler: "silence", matchId: "ember-component.send-action" },
+    { handler: "silence", matchId: "ember-routing.route-router" }
   ]
 };
